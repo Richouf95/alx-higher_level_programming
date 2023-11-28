05a942b111c87d02ef95454e9b4ff33b358ee25b
Richouf95 <wourichouf95@gmail.com>
2023-11-28 15:15:09 -0800

    Fix print alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-listofAlpha = list(range(97,123))
-alphabet = ""
-for i in listofAlpha:
-    alphabet += chr(i)
-print(alphabet)
+for x in range(97, 123):
+    print("{}".format(chr(x)), end='')
---
+++ 0x01-python-if_else_loops_functions/test.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print("{}".format(range(0,5)))
