724f8ff5d953c52b31559aa71a9609a22561ed58
Richouf95 <wourichouf95@gmail.com>
2023-11-28 14:38:37 -0800

    Fix last digit 4

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,8 @@ number = random.randint(-10000, 10000)
 n = number - (int(number / 10) * 10)
 if n > 5:
     print("Last digit of", number, "is", n, "and is greater than 5")
-if digit < 6:
-    if digit == 0:
+if n < 6:
+    if n == 0:
         print("Last digit of", number, "is", n, "and is 0")
     else:
         print("Last digit of", number, "is", n, "and is less than 6 and not 0")
