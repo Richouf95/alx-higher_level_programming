9f5d7560a93c4a496d72e8b924952a749ff425aa
Richouf95 <wourichouf95@gmail.com>
2023-11-28 17:12:40 -0800

    Fix wqcharacter is upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    return ord(c) >= ord('A') and ord(c) <= ord('Z')
+def uppercase(str):
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            character = chr(ord(c) - 32)
+            print("{}".format(character), end='')
+print("")
