2b7ff71fd794097a5f9ffe2850bef6347c7c2776
Richouf95 <wourichouf95@gmail.com>
2023-11-29 09:59:47 -0800

    Remove char at

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,2 +1,14 @@
-#!/usr/bin/pyhton3
-
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    i = 0
+    copy = ""
+    if n < 0 and n > len(str):
+        copy = str
+        return copy
+    while i < len(str):
+        x = i
+        i += 1
+        if x == n:
+            continue
+        copy += str[x]
+    return copy
