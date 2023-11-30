d231925512638cb839432b94cee01bc8e2060deb
Richouf95 <wourichouf95@gmail.com>
2023-11-30 13:30:22 -0800

    Fix add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a = 1
 b = 2
-addition = add_0.add(a, b)
 
-print("{} + {} = {}".format(a, b, addition))
+print("{} + {} = {}".format(a, b, add(a, b)))
