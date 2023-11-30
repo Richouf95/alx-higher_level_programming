9d4a5de5fe05a0d57455a55f27c98849af553079
Richouf95 <wourichouf95@gmail.com>
2023-11-30 14:26:19 -0800

    Fix args 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,16 +3,14 @@
 if __name__ == "__main__":
     from sys import argv as arg
     i = 1
-    length = len(arg)
-    if length == 1:
-        print("0 : arguments.")
-    if length == 2:
-        print("{} : arguments:".format(length - 1))
-        while i < length:
-            print("{}: {}".format(i, arg[i]))
-            i += 1
-    if length > 2:
-        print("{} : arguments:".format(length - 1))
-        while i < length:
-            print("{}: {}".format(i, arg[i]))
-            i += 1
+    length = len(arg) - 1
+    if length == 0:
+        print("0 arguments.")
+    elif length == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(length))
+
+    while i <= length:
+        print("{}: {}".format(i, arg[i]))
+        i += 1
