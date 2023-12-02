b3a217ff7cdd5dbdd8e88c1ecf338a961f47285b
Richouf95 <wourichouf95@gmail.com>
2023-12-01 16:41:20 -0800

    new in the list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+
+    if idx < 0:
+        return my_list
+    elif idx > length - 1:
+        return my_list
+    else:
+        newList = my_list.copy()
+        newList[idx] = element
+        return newList
