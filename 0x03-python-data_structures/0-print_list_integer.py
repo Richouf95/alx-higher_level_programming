5818ff7b40e784c9bb820a3c985f8c7c5c450d49
Richouf95 <wourichouf95@gmail.com>
2023-12-01 16:18:48 -0800

    Get element at index(x)

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/pyhton3
 
 def print_list_integer(my_list=[]):
     for x in my_list:
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    length = len(my_list)
+
+    if idx < 0:
+        return None
+    elif idx > length - 1:
+        return None
+    else:
+        return my_list[idx]
