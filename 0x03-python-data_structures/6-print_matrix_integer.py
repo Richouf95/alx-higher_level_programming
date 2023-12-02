c7045a4223b8390695a00d36769ab5f0c4606bad
Richouf95 <wourichouf95@gmail.com>
2023-12-02 09:21:03 -0800

    Fix 6-print_matrix_integer.py 5

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for x in matrix:
             for y in x:
-                print("{}".format(y), end=" " if y != x[-1] else "")
+                print("{:d}".format(y), end=" " if y != x[-1] else "")
             print()
     else:
         print()
