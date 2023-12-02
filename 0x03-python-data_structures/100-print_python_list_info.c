252a6e8006c37189b6b697d244d8fed0355ab85b
Richouf95 <wourichouf95@gmail.com>
2023-12-02 15:46:37 -0800

    Fix Print python list 10

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,15 +7,17 @@
 
 void print_python_list_info(PyObject *p)
 {
-	int i;
-	PyObject *obj;
-	int size = Py_SIZE(p);
+	Py_ssize_t size, i;
+	PyObject *item;
 
-	printf("[*] Size of the Python List = %lu\n", size);
-	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	for (i = 0; i < size; i++)
+	size = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < size; ++i)
 	{
-		obj = Py_TYPE(PyList_GetItem(p, i))->tp_name;
-		printf("Element %d: %s\n", i, obj);
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - check if list is palindrome
+ * @head: list
+ * Return: 0 if ok 1 if not ok
+*/
+
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL || head == NULL=)
+		return (1);
+
+	return (check(head, *head));
+}
+
+/**
+ *
+*/
+
+
