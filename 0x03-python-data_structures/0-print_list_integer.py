10c4b1419ec4a6e88f89eb42dabdb7dc83e325b7
Richouf95 <wourichouf95@gmail.com>
2023-12-02 07:53:26 -0800

    Fix 0-print_list_integer 2

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
     for x in my_list:
-        print("{:d}".format(x), end="\n")
+        print("{}".format(x), end="\n")
