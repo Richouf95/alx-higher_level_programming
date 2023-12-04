dfea2177bea2a2f6a26b7af8395277ef5ccfc422
Richouf95 <wourichouf95@gmail.com>
2023-12-03 23:21:56 -0800

    Fix 13-is_palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,31 @@
 #include "lists.h"
 
 /**
+ * check - check if list is palindrome
+ * @head: list
+ * @current: current node
+ * Return: Result
+*/
+
+int check(listint_t *head, listint_t **current)
+{
+	int tail;
+
+	if (head == NULL)
+		return (1);
+
+	tail = check(head->next, current);
+
+	if (tail && (*current)->data == head->data)
+	{
+		*current = (*current)->next;
+		return (1);
+	}
+	else
+		return (0);
+}
+
+/**
  * is_palindrome - check if list is palindrome
  * @head: list
  * Return: 0 if ok 1 if not ok
@@ -8,14 +33,8 @@
 
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || head == NULL=)
+	if (*head == NULL || head == NULL)
 		return (1);
 
 	return (check(head, *head));
 }
-
-/**
- *
-*/
-
-
