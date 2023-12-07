f38dac39322a4068e3a59610093eb6865a936be4
Richouf95 <wourichouf95@gmail.com>
2023-12-06 16:43:08 -0800

    weight average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    total = 0
+    divider = 0
+    
+    for x in my_list:
+        mul = x[0] * x[1]
+        total += mul
+        divider += x[1]
+
+    return total / divider
