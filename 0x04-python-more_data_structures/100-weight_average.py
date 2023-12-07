0ea1522360d82fba3e8bc8ca41aab38dec568cc5
Richouf95 <wourichouf95@gmail.com>
2023-12-06 16:45:38 -0800

    Fix weight average 1

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,6 +4,9 @@ def weight_average(my_list=[]):
     total = 0
     divider = 0
 
+    if len(my_list) == 0:
+        return 0
+
     for x in my_list:
         mul = x[0] * x[1]
         total += mul
