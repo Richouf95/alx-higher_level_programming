7dd5907b03e3defb17dfa188339dcda79c8494f4
Richouf95 <wourichouf95@gmail.com>
2023-12-18 14:35:00 -0800

    Fix Magic calculator

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,8 +4,8 @@ def magic_calculation(a, b):
     result = 0
     for x in range(1, 3):
         try:
-            result += a ** b / i
-            if i > a:
+            result += a ** b / x
+            if x > a:
                 raise Exception("Too far")
         except Exception as e:
             result = a + b
