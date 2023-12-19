cc5c2d569cd0f0b02499db3d97cd3b642c8d40c7
Richouf95 <wourichouf95@gmail.com>
2023-12-19 13:18:13 -0800

    Square coordonate

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+"""
+    This is our Square class module
+"""
+
+
+class Square:
+    """
+        This is our Class Square"
+    """
+    def __init__(self, s=0, p=(0, 0)):
+        """
+            Constructor.
+            Args:
+                s: square size
+                position: square position
+        """
+        self.__size = s
+        self.__position = p
+
+    def _getSize(self):
+        """
+            Getter property for size attribute
+        """
+        return self.__size
+
+    def _setSize(self, newSize):
+        """
+            Setter property for size attribute
+        """
+        if not isinstance(newSize, int):
+            raise TypeError("size must be an integer")
+        elif newSize < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = newSize
+
+    def _getPosition(self):
+        """
+            Getter property for Position attribute
+        """
+        return self.__position
+
+    def _setPosition(self, newPosition):
+        """
+            Getter property for Position attribute
+        """
+        if (not isinstance(newPosition, tuple) or
+            len(newPosition) != 2 or
+            not all(isinstance(x, int) for x in newPosition) or
+            not all(x >= 0 for x in newPosition)):
+             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = newPosition
+
+    def area(self):
+        """
+            This function return the current square area
+        """
+        squareArea = self.size ** 2
+        return squareArea
+
+    size = property(_getSize, _setSize)
+    position = property(_getPosition, _setPosition)
+
+    def my_print(self):
+        """
+            Function to print a square
+        """
+        if self.size == 0:
+            return print("")
+
+        for x in range(0, self.position[1]):
+            print("")
+        for y in range(0, self.size):
+            for z in range(0, self.position[0]):
+                print(" ", end="")
+            for a in range(0, self.size):
+                print("#", end="")
+            print("")
