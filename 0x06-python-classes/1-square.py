450d551623188a9b544ffda1c3f2bdabb77ae0c8
Richouf95 <wourichouf95@gmail.com>
2023-12-19 10:32:41 -0800

    Class attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+    This is our Square class module
+"""
+
+
+class Square:
+    """
+        This is our Class Square"
+    """
+    def __init__(self, s):
+        """
+            s: square size
+        """
+        self.size = s
