8ceb8448f553c76b9484a5bccd18f372c2c7dfc1
Richouf95 <wourichouf95@gmail.com>
2024-01-04 14:45:07 -0800

    add two number

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    add two number
+"""
+
+def add_integer(a, b=98):
+    """
+        Function add
+    """
+    if not isinstance(a, int):
+        if not isinstance(a, float):
+            raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        if not isinstance(b, float):
+            raise TypeError("b must be an integer")
+
+    A = int(a)
+    B = int(b)
+
+    return A + B
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
