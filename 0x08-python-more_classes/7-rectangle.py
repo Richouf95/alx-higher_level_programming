7125a25f6c2d941391f43dc91dfa6735343b3310
Richouf95 <wourichouf95@gmail.com>
2024-01-04 12:09:59 -0800

    Fix Make print symbole changable

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -81,7 +81,8 @@ class Rectangle:
 
         rectangle = []
         for i in range(self.__height):
-            [rectangle.append(str(self.print_symbol)) for j in range(self.__width)]
+            [rectangle.append(str(self.print_symbol))
+                for j in range(self.__width)]
             if i != self.__height - 1:
                 rectangle.append("\n")
         return "".join(rectangle)
