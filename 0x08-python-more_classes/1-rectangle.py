a40a50b60c099af65dc00bed76a34a87279b44c5
Richouf95 <wourichouf95@gmail.com>
2024-01-04 08:57:19 -0800

    Real rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+    Define class Rectangle
+"""
+
+
+class Rectangle:
+    """
+        This represente a Rectangle
+    """
+
+    def __init__(self, w=0, h=0):
+        """
+            Initialize a new Rectangle
+
+            Args:
+                w (int): Rectangle width
+                h (int): Rectangle height
+        """
+        self.__width = w
+        self.__height = h
+
+    def _getWidth(self):
+        """
+            Width Getter
+        """
+        return self.__width
+
+    def _setWidth(self, newWidth):
+        """
+            Width Setter
+        """
+        if not isinstance(newWidth, int):
+            raise TypeError("width must be an integer")
+        if newWidth < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = newWidth
+
+    def _getHeight(self):
+        """
+            Height Getter
+        """
+        return self.__height
+
+    def _setHeight(self, newHeight):
+        """
+            Height Setter
+        """
+        if not isinstance(newHeight, int):
+            raise TypeError("height must be an integer")
+        if newHeight < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = newHeight
+
+    width = property(_getWidth, _setWidth)
+    height = property(_getHeight, _setHeight)
