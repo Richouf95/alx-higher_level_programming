d618e00f692c0cb724b2e84a898fb04741f68067
Richouf95 <wourichouf95@gmail.com>
2024-01-08 03:37:22 -0800

    [Exception] area() is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Geometry module
+"""
+
+
+class BaseGeometry:
+    """
+        Base Geometry
+    """
+
+    def area(self):
+        """
+            Geometry area
+        """
+        raise Exception("area() is not implemented")
