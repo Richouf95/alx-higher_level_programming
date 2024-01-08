fe97a921c31997869fd88bb1c0c186256fc81918
Richouf95 <wourichouf95@gmail.com>
2024-01-08 04:09:18 -0800

    Fix Integer validator 2

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,7 +19,7 @@ class BaseGeometry:
         """
             Integer validator
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
