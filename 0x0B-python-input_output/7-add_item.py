6439fcd1ecfd8e20bcb93a8eea32fa033770076c
Richouf95 <wourichouf95@gmail.com>
2024-01-09 10:50:25 -0800

    Load add and save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Load, add, save
+"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    data = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    data = []
+data.extend(sys.argv[1:])
+save_to_json_file(data, "add_item.json")
