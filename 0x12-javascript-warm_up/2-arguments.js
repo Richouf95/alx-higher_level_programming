943b7143d469be40a77ddeb3009848efbefa1723
Richouf95 <wourichouf95@gmail.com>
2024-03-11 07:41:20 -0700

    fix 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,15 +4,10 @@ const { argv } = require('node:process');
 
 const argvSize = argv.length;
 
-if (argvSize === 3)
-{
-	console.log('Argument found');
-}
-else if (argvSize > 3)
-{
-	console.log('Arguments found');
-}
-else
-{
-	console.log('No argument');
+if (argvSize === 3) {
+  console.log('Argument found');
+} else if (argvSize > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
 }
