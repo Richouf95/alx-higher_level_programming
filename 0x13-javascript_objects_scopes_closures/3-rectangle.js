82cd1f84ca31267ae6b7c3cd971d304d42dd7a30
Richouf95 <wourichouf95@gmail.com>
2024-03-12 16:04:11 -0700

    print rectangle(

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+	print () {
+		for (let y = 0; y < this.height; y++) {
+			let line = '';
+			for (let x = 0; x < this.width; x++) {
+				line += 'x';
+			}
+			console.log(line);
+		}
+	}
+}
+
+module.exports = Rectangle;
