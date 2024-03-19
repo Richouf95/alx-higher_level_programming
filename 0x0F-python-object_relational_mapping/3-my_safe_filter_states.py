e5325bfdbd50b3a83ee87bcf8f74fa5cc288f03a
Richouf95 <wourichouf95@gmail.com>
2024-03-19 16:46:43 -0700

    Safe filter user input query

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    dataBase = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+            )
+    cursor = dataBase.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY %s ORDER BY states.id""", (sys.argv[4], ))
+    rows = cursor.fetchall()
+
+    for r in rows:
+        print(r)
+
+    cursor.close()
+    dataBase.close()
