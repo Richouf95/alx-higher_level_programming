33e9dbf0ab2edef038852825bdab942dee3b5cea
Richouf95 <wourichouf95@gmail.com>
2024-03-20 14:50:27 -0700

    Make on-to-many relation betwen two table

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+class definition of a City
+"""
+
+
+from relationship_state import Base
+from sqlalchemy import (
+        create_engine, Column, Integer, String, MetaData, ForeignKey
+        )
+
+
+class City(Base):
+    """
+    Class that define City data structure
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
