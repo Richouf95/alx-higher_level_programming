8436502801afa4e07816104f02de9fdf723c73f2
Richouf95 <wourichouf95@gmail.com>
2024-03-20 09:01:15 -0700

    get Id with a state by his name

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+using the module SQLAlchemy
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3]
+                ), pool_pre_ping=True
+            )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == (sys.argv[4], ))
+
+    try:
+        print(state[0].id)
+    except IndexError:
+        print("Not found")
