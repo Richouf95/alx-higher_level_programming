51bf1002f1232919870ab101cdd277d673c96fa5
Richouf95 <wourichouf95@gmail.com>
2024-03-28 19:14:39 -0700

    Get error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+sends a request to the URL and displays the body of the response
+"""
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
