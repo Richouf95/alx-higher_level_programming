f29530f6992ea08f7275781de85a06b86c19c0af
Richouf95 <wourichouf95@gmail.com>
2024-03-28 18:28:52 -0700

    Fix hbtn_status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
+"""
+fetches https://alx-intranet.hbtn.io/status.
 """
 
 
 if __name__ == '__main__':
     import urllib.request
 
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as req:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as req:
         res = req.read()
         print("Body response:")
         print("    - type: {}".format(type(res)))
