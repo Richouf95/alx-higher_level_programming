0c6ad781d8f33a4f0df0507792b8e8c29068fce5
Richouf95 <wourichouf95@gmail.com>
2024-03-28 19:46:03 -0700

    hbtn_status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ fetches https://alx-intranet.hbtn.io/status.
 """
 
 
-if __name__ == '__main__':
-    import urllib.request
+import urllib.request
 
+if __name__ == '__main__':
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as req:
         res = req.read()
         print("Body response:")
