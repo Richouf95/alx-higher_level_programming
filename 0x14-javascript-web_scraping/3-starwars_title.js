16d7e28823eecff73eaf776a9ccd0a44d003ea50
Richouf95 <wourichouf95@gmail.com>
2024-04-23 16:45:52 -0700

    Fix starwars_title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,4 +6,5 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request.get(url, (err, res, body) => {
   if (!err) console.log(JSON.parse(body).title);
+  else console.log(err);
 });
