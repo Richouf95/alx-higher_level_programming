e651317fd534608f2bcf3507e4a2d80501c9c5f9
Richouf95 <wourichouf95@gmail.com>
2024-04-23 18:43:45 -0700

    Right order starwars_characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,10 +7,14 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request.get(url, (err, res, body) => {
   if (!err && res.statusCode === 200) {
     const characters = JSON.parse(body).characters;
-    characters.forEach(character => {
-      request.get(character, (err, res, body) => {
-        if (!err && res.statusCode === 200) console.log(JSON.parse(body).name);
-      });
+    characters.map(item => {
+      return new Promise((resolve, reject) => {
+        request.get(item, (error, response, characterbody) => {
+          if (!err && res.statusCode === 200) {
+            resolve(JSON.parse(characterbody).name);
+          } else reject(new Error(error));
+        });
+      }).then(c => console.log(c));
     });
-  }
+  } else console.error(err);
 });
