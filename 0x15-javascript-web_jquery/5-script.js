c9d2886280471a2976b770e4e84a1450764b7fb5
Richouf95 <wourichouf95@gmail.com>
2024-05-03 01:33:56 +0100

    List of elements

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,6 @@
+// JavaScript script that adds a 
+// <li> element to a list when 
+// the user clicks on the tag DIV#add_item:
+$('#add_item').on('click', () => {
+    $('.my_list').append('<li>Item</li>');
+});
